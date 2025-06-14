0ad7485ada807196821e2b644dd65e9294a7dd06
Sleepkaze <kiahanabil@gmail.com>
2025-06-14 10:46:12 +0700

    ea

--- AplikasiBudidaya.java
+++ AplikasiBudidaya.java
@@ -1,28 +1,16 @@
-// AplikasiBudidaya.java
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class AplikasiBudidaya extends Application {
-//tes
     @Override
     public void start(Stage primaryStage) {
-        // Mengatur judul jendela utama aplikasi
         primaryStage.setTitle("Sistem Manajemen Ternak Ikan");
-
-        // Buat objek Budidaya
-        // Objek Budidaya ini sekarang akan berinteraksi dengan DatabaseManager
         Budidaya budidaya = new Budidaya(); 
-        
-        // Buat objek Menu, yang akan mengelola tampilan GUI dan interaksi dengan Budidaya
-        Menu menu = new Menu(budidaya, primaryStage); 
-        
-        // Panggil metode untuk menampilkan menu utama aplikasi
+        Menu menu = new Menu(budidaya, primaryStage);         
         menu.tampilkanMenuUtama(); 
     }
 
     public static void main(String[] args) {
-        // Ini adalah cara untuk meluncurkan aplikasi JavaFX.
-        // launch() akan memanggil metode start() di atas.
         launch(args);
     }
 }
